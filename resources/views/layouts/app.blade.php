3e1bbcb03cc90d855e5df492861e7fc8ad86f52d
Piccolo <kabiryusufbashir@gmail.com>
2022-10-10 16:58:42 +0100

    Create Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,20 +17,19 @@
             <div class="">
                 <a href="#">
                     <div class="lg:col-span-1">
-                        <img class="w-14 lg:w-16 lg:ml-0" src="{{ asset('images/bowdi.png') }}" alt="BOWDI logo">
+                        <img class="w-14 lg:w-32 lg:ml-0" src="{{ asset('images/logo.png') }}" alt="Karmus Dictionary logo">
                     </div>
                 </a>
             </div>
         </div>
         <div class="lg:col-span-4 hidden lg:block">
-            <nav class="lg:flex justify-between list-none uppercase font-medium">
-                <li class="py-1 "><a href="#">Home</a></li>
-                <li class="py-1 "><a href="#">NUMBERS</a></li>
-                <li class="py-1 "><a href="#">PRONUNCIATION</a></li>
-                <li class="py-1 "><a href="#">PROVERBS</a></li>
-                <li class="py-1 "><a href="#">FAQS</a></li>
-                <li class="py-1 "><a href="#">ABOUT KAMUS</a></li>
-                <li class="py-1 "><a href="#">CONTACT US</a></li>
+            <nav class="lg:flex justify-between list-none uppercase font-medium items-center">
+                <li class="py-1"><a href="#">Home</a></li>
+                <li class="py-1"><a href="#">NUMBERS</a></li>
+                <li class="py-1"><a href="#">PRONUNCIATION</a></li>
+                <li class="py-1"><a href="#">PROVERBS</a></li>
+                <li class="py-1"><a href="#">CONTRIBUTE</a></li>
+                <li class=" bg-red-700 px-5 py-3 rounded-full"><a href="#">DONATE</a></li>
             </nav>
         </div>
     </div>
@@ -96,6 +95,116 @@
         </div>
     </div>
     <!-- End of Navigation Bar  -->
+
 	<div id="app"></div>
+
+    <!-- Footer  -->
+    <div class="relative top-24">
+        <div id="footer" class=" bg-green-600 py-12 px-8 lg:px-24 lg:grid grid-cols-5 gap-8 text-white">
+            <div class="col-span-2">
+                <div>
+                    <img class="w-28 mx-auto lg:mx-0 mb-6" src="{{ asset('images/logo.png') }}" alt="Karmus Dictionary Logo">
+                    <h1 class="text-3xl font-bold mb-4">Office Address</h1>
+                    <p class="py-1 text-left lg:w-2/3 w-full">
+                        No 7 Kabiru Alhaji Bashir Street off Maiduguri Road, Kano State Nigeria
+                    </p>
+                </div>
+                <div class="mt-4">
+                    <h1 class="text-3xl font-bold mb-4">Karin Magana</h1>
+                    <p class="py-1 lg:w-2/3 w-full text-justify list-item ml-6">Da rarrafe yaro ya kan tashi</p>
+                    <p class="py-1 lg:w-2/3 w-full text-justify list-item ml-6">Duk wanda ya daka rawar wani, zai rasa turmin daka tasa</p>
+                    <p class="py-1 lg:w-2/3 w-full text-justify list-item ml-6">Ba girin-girin ba, ta yi mai</p>
+                </div>
+            </div>
+            <div class="col-span-2">
+                <h1 class="text-3xl font-bold mb-4">Navigations</h1>
+                <nav class="list-none text-white">
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>Home</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>Numbers</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>Pronunciation</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>Proverbs</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>FAQs</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>About Karmus</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>Sign Up</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>Sign In</span>
+                        </a>
+                    </li>
+                    <li class="py-1 hover:text-gray-800">
+                        <a class="flex items-center py-1" href="#">
+                            <span>Contact Us</span>
+                        </a>
+                    </li>
+                </nav>
+            </div>
+            <div class="col-span-1">    
+                <div>
+                    <h1 class="text-3xl font-bold mb-4">Follow Us</h1>
+                    <nav class="list-none text-white">
+                        <li class="py-1 hover:text-gray-800">
+                            <a class="flex items-center py-1" href="https://twitter.com/bornowomendevt">
+                                <span><i class="fa-brands fa-twitter text-xl"></i></span> &nbsp;&nbsp;
+                                <span>Twitter</span>
+                            </a>
+                        </li>
+                        <li class="py-1 hover:text-gray-800">
+                            <a class="flex items-center py-1" href="https://web.facebook.com/bornowomen/">
+                                <span><i class="fa-brands fa-facebook text-xl"></i></span> &nbsp;&nbsp;
+                                <span>Facebook</span>
+                            </a>
+                        </li>
+                        <li class="py-1 hover:text-gray-800">
+                            <a class="flex items-center py-1" href="https://www.linkedin.com/company/bowdi/">
+                                <span><i class="fa-brands fa-linkedin text-xl"></i></span> &nbsp;&nbsp;
+                                <span>LinkedIn</span>
+                            </a>
+                        </li>
+                        <li class="py-1 hover:text-gray-800">
+                            <a class="flex items-center py-1" href="#">
+                                <span><i class="fa-brands fa-instagram text-xl"></i></span> &nbsp;&nbsp;
+                                <span>Instagram</span>
+                            </a>
+                        </li>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="py-7 text-center sm:text-sm">
+            <footer>
+                Founded by <a class="hover:text-green-600" href="https://bashirabdul.teampiccolo.com/">Bashir Abdul (@Sabash3k)</a><br>
+                A Product of <a class="hover:text-green-600" href="https://teampiccolo.com">Team Piccolo</a><br>
+                Karmus Dictionary Copyright © 2019-@php echo date('Y') @endphp All Rights Reserved 
+            </footer>
+        </div>
+    </div>
 </body>
 </html>
