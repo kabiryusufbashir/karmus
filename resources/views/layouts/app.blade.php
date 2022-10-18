561620da289a726241dfbd53dc55459d73f5cde5
Piccolo <kabiryusufbashir@gmail.com>
2022-10-18 09:53:16 +0100

    Remove livewire and jetstream

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @livewireStyles
 </head>
 <body>
     <!-- Navigation  -->
@@ -258,6 +257,5 @@
             </div>     
         </div>
     </div>
-    @livewireScripts
 </body>
 </html>
